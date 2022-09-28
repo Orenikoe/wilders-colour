732bf628d85fac05f8fd48d4f558e3da710fcf53
Orenikoe <orental21@gmail.com>
2022-09-28 15:11:10 +0100

    1st commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.bgCyanBright.redBright("Da Silva"));
+console.log(chalk.bgGreenBright.greenBright("Amanda"));
+console.log(chalk.bgBlackBright.blackBright("Andreas"));
+console.log(chalk.bgMagentaBright.magentaBright("Carla"));
